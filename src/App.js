f9b0521da920929ceabd0c61bce095fbcb5a8d5e
natia <nati.shonia37@gmail.com>
2022-09-15 13:18:25 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { Contact } from './components/Contact';
+import Experience from './components/Experience';
+import { Footer } from './components/Footer';
+import Intro from './components/Intro';
+import Navbar from './components/Navbar';
+import Portfolio from './components/Portfolio';
+import Services from './components/Services'
+import { Testimonial } from './components/Testimonial';
+import Works from './components/Works';
+import {themeContext} from './Context'
+import {useContext} from 'react'
+
+
 
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}
+    >
+       <Navbar />
+       <Intro />
+       <Services/>
+       <Experience/>
+      <Works/>
+      <Portfolio/>
+      <Testimonial/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
